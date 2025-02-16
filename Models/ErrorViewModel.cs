bd25d0cf1f95d8d4dbedf575e74447a5c049876e
pragatiwork <pragatiwork93@gmail.com>
2025-02-16 09:59:01 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExpenseSmart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/ExpensesDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseSmart.Models
+{
+    public class ExpensesDBContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+        public ExpensesDBContext(DbContextOptions<ExpensesDBContext> options)
+             : base(options)
+        {
+                
+        }
+    }
+}
